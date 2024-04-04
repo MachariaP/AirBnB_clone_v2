6b1e4653aff796e2b33420f85ea85f6a5fb28dd4
Phinehas <walburphinehas78@gmail.com>
2024-04-04 10:41:55 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
@@ -7,7 +6,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
-
+from os import getenv
+import hashlib
 
 class User(BaseModel, Base):
     """This is the class for user
@@ -26,44 +26,6 @@ class User(BaseModel, Base):
                           backref="user")
     reviews = relationship("Review", cascade='all, delete, delete-orphan',
                            backref="user")
-=======
-""" holds class User"""
-import hashlib
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-
-
-class User(BaseModel, Base):
-    """Representation of a user """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
 
     @property
     def password(self):
@@ -73,4 +35,3 @@ class User(BaseModel, Base):
     def password(self, pwd):
         """hashing password values"""
         self._password = pwd
->>>>>>> refs/remotes/origin/master
