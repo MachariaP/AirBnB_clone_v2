22af9e5932c29b5ca0328f7b2d509a8b4a9a7afe
MachariaP <walburphinehas78@gmail.com>
2024-04-09 11:53:29 +0300

    modified:   0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,13 +14,13 @@ sudo echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+    <h1>Holberton School</h1>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s  /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu: /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
