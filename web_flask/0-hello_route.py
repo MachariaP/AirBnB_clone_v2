ef0ce8ec09ed152674fb17efd859bb7be8a33bf8
MachariaP <walburphinehas78@gmail.com>
2024-04-19 23:42:51 +0300

    a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
-#!/user/bin/env python3
-""" Module that starts a Flask web application """
-
+#!/usr/bin/python3
+"""
+start Flask application
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """ Display a message """
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
